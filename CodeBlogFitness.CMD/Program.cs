0bea3effc25095779944e06d397fdedd10c401b1
Пасько Виталий <pasko.vitaliy24@gmail.com>
2021-04-04 12:39:06 +0600

    View Добавил взаимодействие с пользователем. Добавил gitignore

--- CodeBlogFitness.CMD/Program.cs
+++ CodeBlogFitness.CMD/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CodeBlogFitness.BL.Controller;
 
 namespace CodeBlogFitness.CMD
 {
@@ -9,6 +10,16 @@ namespace CodeBlogFitness.CMD
             Console.WriteLine("Вас приветствует приложение CodeBlogFitness");
             Console.WriteLine("Введите имя пользователя");
             var name = Console.ReadLine();
+            Console.WriteLine("Введите пол");
+            var gender = Console.ReadLine();
+            Console.WriteLine("Введите ДР");
+            var birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Введите вес");
+            var weight = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите рост");
+            var height = double.Parse(Console.ReadLine());
+            var userController = new UserController(name, gender, birthDate, weight, height);
+            userController.Save();
         }
     }
 }
